ea90055530ece1b26b3fd30d6d8b1d9b6232a6dd
NicoField <74797059+NicoField@users.noreply.github.com>
2023-05-24 13:32:26 -0400

    Update streamlit_app.py
    Adding menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title("New Stream")
-streamlit.header("Header 1")
-streamlit.text("Text 1")
-streamlit.text("Text 2")
-streamlit.header("Header 2")
-streamlit.text("Text 3")
-streamlit.text("Text 4")
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach, and Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
