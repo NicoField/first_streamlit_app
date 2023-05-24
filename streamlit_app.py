924aa0739e8e21f7a2410ae0bfbebc1215ed5f8e
NicoField <74797059+NicoField@users.noreply.github.com>
2023-05-24 13:27:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("New Stream")
+streamlit.header("Header 1")
+streamlit.text("Text 1")
+streamlit.text("Text 2")
+streamlit.header("Header 2")
+streamlit.text("Text 3")
+streamlit.text("Text 4")
